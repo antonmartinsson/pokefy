6e099bfb906d3d2fb767b30db7db9178ba125590
antonmedstorta <aejmartinsson@gmail.com>
2018-11-24 14:17:40 +0100

    Pretty much done with selecting random Pokémon from type

--- pokefy/src/modules/fetchTypes.js
+++ pokefy/src/modules/fetchTypes.js
@@ -13,15 +13,15 @@ async function getDetails(randPokemon) {
     return json;
 }
 
-
 class TypeComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
-      items: [],
+      name: '',
       type: '',
+      image: '',
     };
   }
 
@@ -31,14 +31,40 @@ class TypeComponent extends React.Component {
         const typeRes = await getType(this.state.type)
         var pokeAmount = typeRes.pokemon.length
         var randPokemonID = Math.round(Math.random() * (pokeAmount-1) + 0)
+
+        //Select the name of a random Pokémon from the type list
         var randPokemon = typeRes.pokemon[randPokemonID].pokemon.name
-        const pokemon = await getDetails(randPokemon);
-        console.log(pokemon);
         
-        this.setState ({
+        // Make another API-call to get the details for that Pokémon
+        const pokemon = await getDetails(randPokemon);
+        var sprite = pokemon.sprites.front_default;
+
+        // If the Pokémon doesn't have a sprite, just get another Pokémon.
+        while (sprite == undefined) {
+          const pokemon = await getDetails(randPokemon);
+          var sprite = pokemon.sprites.front_default;
+        }
+
+        console.log(sprite);
+        var pokemonCap = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        
+        if (pokemonCap.includes("-")) {
+          var pokemonClean = pokemonCap.substring(0, pokemonCap.indexOf('-'));
+          this.setState ({
             isLoaded: true,
-            items: pokemon
+            name: pokemonClean,
+            image: sprite
         })
+        }
+
+        else {
+          this.setState ({
+            isLoaded: true,
+            name: pokemonCap,
+            image: sprite
+        })
+        }
+        
     }
     catch(error) {
         console.log(error);
@@ -56,18 +82,25 @@ class TypeComponent extends React.Component {
   }
 
   render() {
+    // Create the content
     let content;
     const { error, isLoaded, items } = this.state;
     if (error) {
       content = <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
-      content = <div>Loading...</div>;
+      //content = <div>Loading...</div>;
     } else {
       content = (
-        <div>{items.name}</div>
+        <div>
+          <img src={this.state.image} width='200px' height='200px' margin-bottom='-50px'/>
+          <br/>
+          {this.state.name}
+        </div>
+
       );
     }
 
+    // Render the content
     return (
         <div>
             <form onSubmit={this.getPokemon}>
