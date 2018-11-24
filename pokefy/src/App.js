be88d54925ecb4f73ee62ea93c9b5e616736b4d4
antonmedstorta <aejmartinsson@gmail.com>
2018-11-24 11:51:13 +0100

    Able to get Pokémon

--- pokefy/src/App.js
+++ pokefy/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TypeComponent from './modules/fetchTypes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <TypeComponent/>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
---
+++ pokefy/src/modules/fetchTypes.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+async function getType(type) {
+    const typeRes = await fetch('http://pokeapi.co/api/v2/type/' + type)
+    const json = await typeRes.json();
+    return json;
+}
+
+async function getDetails(randPokemon) {
+    const pokemonRes = await fetch('http://pokeapi.co/api/v2/pokemon/' + randPokemon)
+    const json = await pokemonRes.json();
+    return json;
+}
+
+
+class TypeComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      type: '',
+    };
+  }
+
+  getPokemon = async (event) => {
+    event.preventDefault()
+    const typeRes = await getType(this.state.type)
+    var pokeAmount = typeRes.pokemon.length
+    var randPokemonID = Math.round(Math.random() * (pokeAmount-1) + 0)
+    var randPokemon = typeRes.pokemon[randPokemonID].pokemon.name
+
+    const pokemon = await getDetails(randPokemon);
+    console.log(pokemon);
+      
+  }
+
+  onChange = (event) => {
+    this.setState({type: event.target.value});
+  }
+
+  render() {
+    let content;
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      content = <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      content = <div>Loading...</div>;
+    } else {
+      content = (
+        <div>{items.name}</div>
+      );
+    }
+
+    return (
+        <div>
+            <form onSubmit={this.getPokemon}>
+                <input onChange={this.onChange} value={this.state.type} type="text" id="type" />
+            </form>
+            {content}
+        </div>
+    )
+  }
+}
+
+export default TypeComponent;
