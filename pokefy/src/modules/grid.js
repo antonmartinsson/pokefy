c305ea1a57f3ddf5639804ec0c22911340ac5f5b
Willy Liu <arkazain@gmail.com>
2018-11-24 16:31:11 +0100

    Fix grid

--- pokefy/src/modules/grid.js
+++ pokefy/src/modules/grid.js
@@ -1,37 +1,38 @@
 import React, { Component } from 'react';
 import '../App.css';
-import GridItem from "./grid-item";
+import GridItem from './grid-item';
+import './grid.css';
 
-class Grid extends Component{
+class Grid extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      readyForGame: false,
+    };
+  }
 
-        this.state = {
-            readyForGame: false,
+  render() {
+    const { tracks } = this.props;
+    console.log(tracks);
 
-        };
-
-    }
-
-    render(){
-        const { tracks } = this.props;
-        console.log(tracks);
-
-        return (
-            <div>
-                <h3>{this.state.title}</h3>
-                <br/>
-                <div className="song-grid">
-                {
-                    tracks.slice(0, 10).map(track => (
-                            <GridItem track={track} key={track.played_at} moveToGame={this.props.action} ready={this.props.readyForGame}/>
-                    ))
-                }
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h3>Pick a song!</h3>
+        <br />
+        <div className='song-grid'>
+          {tracks.slice(0, 10).map(track => (
+            <GridItem
+              track={track}
+              key={track.played_at}
+              moveToGame={this.props.action}
+              ready={this.props.readyForGame}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Grid;
